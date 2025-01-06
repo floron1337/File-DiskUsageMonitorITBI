e425f7e231fa68b49fa3ffaf0d047330b41e03af
ch4nc4 <raluca-melinda.ionete@s.unibuc.ro>
2025-01-06 17:09:27 +0200

    find first ls -l command in file

--- parse_typescript.sh
+++ parse_typescript.sh
@@ -19,12 +19,21 @@ cleaned_file=$(mktemp)
 cat "$typescript_file" | sed 's/\x1b\[[0-9;]*[a-zA-Z]//g' > "$cleaned_file"
 
 # Extract outputs of all `ls -l` commands
-ls_outputs=$(awk '/ls -l/{flag=1; next} flag && /^[^$]/ {print; next} /^[[:space:]]*$/ {flag=0}' "$cleaned_file")
-
+# ls_outputs=$(awk '/ls -l/{flag=1; next} flag && /^[^$]/ {print; next} /^[[:space:]]*$/ {flag=0}' "$cleaned_file")
+# ls_outputs=$(awk '/ls -l/ {flag=1; print; next} flag && /^[^$]/ {print; next} /^[[:space:]]*$/ {flag=0}' "$cleaned_file")
+ls_outputs=$(awk '{if (/ls -l/) print "ls -l"; else print}' "$cleaned_file")
 # Get the first and last `ls -l` outputs
-parsed_output=$(echo "$ls_outputs" | awk 'NR==1,/^$/')
+# parsed_output=$(echo "$ls_outputs" | awk 'NR==1,/^$/')
+
+# Get the first `ls -l` output block
+# first_output=$(echo "$ls_outputs" | awk '/ls -l/,/ls -l/')
+# first_output=$(echo "$ls_outputs" | awk '/ls -l/ {found=1} found && /floron@/ {exit} found {print}')
+# first_output=$(echo "$ls_outputs" | awk '/ls -l/ {found=1} found && /floron@/ {exit} found && /ls -l/ {exit} found {print}')
+first_output=$(echo "$ls_outputs" | awk 'BEGIN {found=0} /ls -l/ && !found {found=1; print; next} found && /ls -l/ {exit} found && /floron@/ {exit} found {print}')
 
 # Clean up temporary file
 rm -f "$cleaned_file"
 
-echo "$parsed_output"
+echo "First ls -l output:"
+echo "$first_output"
+# echo "$parsed_output"
