b66374f170bed4998c6e6bdcad22d81c377a6149
NoBullyy <76968027+NoBullyy@users.noreply.github.com>
2025-01-10 10:51:08 +0200

    am adaugat diskusage si userfriendly print ls_difftypescript.sh
    dif primu ultimu ls, dif ultimul ls ls curent, disk usage

--- ls_difftypescript.sh
+++ ls_difftypescript.sh
@@ -34,7 +34,7 @@ current_line=1
 # Read the variable line by line
 while IFS= read -r line; do
     # Check if the line contains an ls -l command
-    if [[ "$line" == *"ls -l"* ]]; then
+    if [[ "$line" == "ls -l" ]]; then
         last_ls_output=$(echo "$ls_outputs" | awk -v start_line="$current_line" '
         NR >= start_line && !found && /ls -l/ {found=1; print; next}
         NR >= start_line && found && /ls -l/ {exit}
@@ -46,12 +46,29 @@ while IFS= read -r line; do
     ((current_line++))
 done <<< "$ls_outputs"
 
+# Extract outputs of the 'df' command from the typescript file
+df_outputs=$(awk '{if (/df/) print "df"; else print}' "$cleaned_file")
+
+# Get the first 'df' output block
+first_df_output=$(echo "$df_outputs" | awk 'BEGIN {found=0} /df/ && !found {found=1; print; next} found && /df/ {exit} found && /@/ {exit} found {print}')
+
+# Get the last 'df' output block
+last_df_output=$(echo "$df_outputs" | awk 'BEGIN {found=0} /df/ {found=1; next} found && /df/ {last=$0; next} found {last=$0} END {print last}')
+
+# Remove the first line headers from the outputs
+first_df_output=$(echo "$first_df_output" | sed '1d')
+last_df_output=$(echo "$last_df_output" | sed '1d')
+
+# Store the difference between first and last 'df' outputs
+df_diff_output=$(diff -u -U0 <(echo "$first_df_output") <(echo "$last_df_output"))
+
 # Clean up temporary file
 rm -f "$cleaned_file"
 
 # Remove the first two lines from the outputs
 first_output=$(echo "$first_output" | sed '1,2d')
 last_ls_output=$(echo "$last_ls_output" | sed '1,2d')
+
 
 # Remove the "EOF reached" message from last_ls_output if it exists
 last_ls_output=$(echo "$last_ls_output" | sed '/EOF reached/d')
@@ -105,8 +122,50 @@ fi
 
 echo 
 
+
+# Display changes in disk usage
+echo
+echo " --------------------------------------------- "
+echo " Disk Usage Analysis"
+echo " --------------------------------------------- "
+
+
+df_diff_output=$(echo "$df_diff_output" | grep -v "^---")
+
+
+if [ -z "$df_diff_output" ]; then
+    echo "No changes in disk usage detected."
+else
+    echo "Differences in disk usage:"
+    echo "$df_diff_output" | grep -E '^[-+]' | grep -v '^+++' | awk '{print $0}'
+fi
+
+echo 
+echo
+
+
 echo " --- Difference between the last ls -l command in this typescript file ---"
 echo " --- and the current ls -l command ---"
 echo
-echo " $diff_output_last_current"
 
+deleted_files2=$(echo "$diff_output_last_current" | grep "^-" | grep -v "^---" | awk '{print $NF}')
+
+added_files2=$(echo "$diff_output_last_current" | grep "^+" | grep -v "^+++" | awk '{print $NF}')
+
+echo "Deleted files:"
+echo
+if [ -z "$deleted_files2" ]; then
+    echo "No deleted files."
+else
+    echo "$deleted_files2"
+fi
+
+echo
+
+echo "Added files:"
+echo
+if [ -z "$added_files2" ]; then
+    echo "No added files."
+else
+    echo "$added_files2"
+fi
